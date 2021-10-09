e48f4478505c6093d9fd2829df2c1611dae86534
Jacob McCarthy <thejacobmccarthy@gmail.com>
2021-10-09 10:55:32 -0400

    logans changes

--- script.js
+++ script.js
@@ -11,10 +11,16 @@ var movieLang = document.getElementById("movie-language");
 var nextButton = document.getElementById("next-movie");
 var prevButton = document.getElementById("previous-movie");
 var current = 0;
-
+var genreSelection = "";
 
 function getApiCallOne(){
-    var genreSelection = genreList.value
+    
+    if(genreList.value !== genreSelection) {
+        current = 0;
+    }
+
+    genreSelection = genreList.value
+
     var apiUrl = "https://api.themoviedb.org/3/movie/now_playing?with_genres=" + genreSelection +"&reigon=840&language=en-US&api_key=9fb4cfc619ac245c369683b5ddd346ed"
     
 
@@ -26,19 +32,19 @@ function getApiCallOne(){
             console.log(data)
             // current = 0
             console.log(current)
-            function displayInfo() {
-                var posterPath = "https://image.tmdb.org/t/p/original/" + data.results[current].poster_path;
-                moviePoster.setAttribute("src", posterPath);
-                movieTitle.innerHTML = data.results[current].original_title;
-                movieDescription.innerHTML = data.results[current].overview;
-                movieReleaseDate.innerHTML = data.results[current].release_date;
-                movieRating.innerHTML = data.results[current].vote_average;
-                movieLang.innerHTML = data.results[current].original_language;
-            }
-            displayInfo()
+            displayInfo(data)
             
 
         })
+}
+function displayInfo(data) {
+    var posterPath = "https://image.tmdb.org/t/p/original/" + data.results[current].poster_path;
+    moviePoster.setAttribute("src", posterPath);
+    movieTitle.innerHTML = data.results[current].original_title;
+    movieDescription.innerHTML = data.results[current].overview;
+    movieReleaseDate.innerHTML = data.results[current].release_date;
+    movieRating.innerHTML = data.results[current].vote_average;
+    movieLang.innerHTML = data.results[current].original_language;
 }
 function nextMovie() {
     current++
